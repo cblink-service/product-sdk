fc5424694c3a44dcbcfe9fc3555cef94bf6262a2
eleven <eleven@cblink.net>
2023-02-01 09:38:55 +0800

    feat:新增sku最新采购价列表接口

--- src/Stock/Client.php
+++ src/Stock/Client.php
@@ -89,6 +89,6 @@ class Client extends BaseApi
      */
     public function skuPurchasePriceList(array $query = [])
     {
-        return $this->httpGet('/stock-order/sku/purchase/price', $query);
+        return $this->httpGet('/stock/order/sku/purchase/price', $query);
     }
 }
